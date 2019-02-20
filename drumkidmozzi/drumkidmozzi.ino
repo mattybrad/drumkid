f54af380636019f07a6a21d72ffc4f86ef672371
Matt Bradshaw <matthewloydbradshaw@gmail.com>
2019-02-20 12:51:48 +0000

    low pass filter working

--- drumkidmozzi/drumkidmozzi.ino
+++ drumkidmozzi/drumkidmozzi.ino
@@ -1,19 +1,24 @@
+// include relevant mozzi libraries
 #include <MozziGuts.h>
-#include <Sample.h> // Sample template
-#include "kick.h"
-#include "closedhat.h"
-#include "snare.h"
+#include <Sample.h>
+#include <LowPassFilter.h>
 #include <EventDelay.h>
 #include <mozzi_rand.h>
 
+// include custom sample files
+#include "kick.h"
+#include "closedhat.h"
+#include "snare.h"
+
 #define CONTROL_RATE 64
 
-// use: Sample <table_size, update_rate> SampleName (wavetable)
+// define samples
 Sample <kick_NUM_CELLS, AUDIO_RATE> aSample(kick_DATA);
 Sample <closedhat_NUM_CELLS, AUDIO_RATE> bSample(closedhat_DATA);
 Sample <snare_NUM_CELLS, AUDIO_RATE> cSample(snare_DATA);
 
-// for scheduling sample start
+// define other mozzi things
+LowPassFilter lpf;
 EventDelay kTriggerDelay;
 
 void setup(){
@@ -21,26 +26,29 @@ void setup(){
   aSample.setFreq((float) kick_SAMPLERATE / (float) kick_NUM_CELLS);
   bSample.setFreq((float) closedhat_SAMPLERATE / (float) closedhat_NUM_CELLS);
   cSample.setFreq((float) snare_SAMPLERATE / (float) snare_NUM_CELLS);
-  aSample.setEnd(7000);
-  kTriggerDelay.set(200);
+  aSample.setEnd(7000); // was getting a funny click at the end of the kick sample
+  lpf.setResonance(200);
+  kTriggerDelay.set(100);
 }
 
 
 void updateControl(){
   if(kTriggerDelay.ready()){
-    byte r = rand(3);
+    byte r = rand(2);
     if(r==0) {
       aSample.start();
     }
-    else if(r==1) bSample.start();
-    else cSample.start();
+    else if(r==1) cSample.start();
+    bSample.start();
     kTriggerDelay.start();
   }
+  lpf.setCutoffFreq(mozziAnalogRead(0)>>2);
 }
 
 
 int updateAudio(){
-  return (int) aSample.next()+bSample.next()+cSample.next();
+  char asig = lpf.next((aSample.next()>>1)+(bSample.next()>>1)+(cSample.next()>>1));
+  return (int) asig;
 }
 
 
