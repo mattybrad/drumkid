bf58e0243e810b991a4d339a8a54773d7b10bd50
Matt Bradshaw <matthewloydbradshaw@gmail.com>
2019-02-22 23:38:01 +0000

    added various buttons

--- drumkidmozzi/drumkidmozzi.ino
+++ drumkidmozzi/drumkidmozzi.ino
@@ -5,12 +5,28 @@
 #include <EventDelay.h>
 #include <mozzi_rand.h>
 
+// include other stuff
+#include <Bounce2.h>
+
 // include custom sample files
 #include "kick.h"
 #include "closedhat.h"
 #include "snare.h"
 
+// define some numbers etc
 #define CONTROL_RATE 64
+#define START_STOP_PIN 2
+#define BUTTON_A_PIN 5
+#define BUTTON_B_PIN 6
+#define BUTTON_C_PIN 7
+#define BUTTON_D_PIN 8
+
+// define buttons
+Bounce startStopButton = Bounce();
+Bounce buttonA = Bounce();
+Bounce buttonB = Bounce();
+Bounce buttonC = Bounce();
+Bounce buttonD = Bounce();
 
 // define samples
 Sample <kick_NUM_CELLS, AUDIO_RATE> aSample(kick_DATA);
@@ -32,6 +48,19 @@ bool beat1[][16] = {  {1,0,0,0,0,0,0,0,1,0,1,0,0,0,0,1,},
                       {0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,},};
 
 void setup(){
+  for(int i=2;i<=8;i++) {
+    pinMode(i,INPUT_PULLUP);
+  }
+  startStopButton.attach(START_STOP_PIN, INPUT_PULLUP);
+  buttonA.attach(BUTTON_A_PIN, INPUT_PULLUP);
+  buttonB.attach(BUTTON_B_PIN, INPUT_PULLUP);
+  buttonC.attach(BUTTON_C_PIN, INPUT_PULLUP);
+  buttonD.attach(BUTTON_D_PIN, INPUT_PULLUP);
+  startStopButton.interval(25);
+  buttonA.interval(25);
+  buttonB.interval(25);
+  buttonC.interval(25);
+  buttonD.interval(25);
   startMozzi(CONTROL_RATE);
   aSample.setFreq((float) kick_SAMPLERATE / (float) kick_NUM_CELLS);
   bSample.setFreq((float) closedhat_SAMPLERATE / (float) closedhat_NUM_CELLS);
@@ -42,9 +71,25 @@ void setup(){
   kTriggerDelay.set(beatTime);
 }
 
-
+bool started = false;
 void updateControl(){
-  if(kTriggerDelay.ready()){
+  bool startNow = false;
+  startStopButton.update();
+  if(startStopButton.fell()) {
+    started = !started;
+    if(started) {
+      beatIndex = 0;
+      startNow = true;
+    }
+  }
+  buttonA.update();
+  buttonB.update();
+  buttonC.update();
+  buttonD.update();
+  if(startStopButton.fell()) {
+    // do something
+  }
+  if((kTriggerDelay.ready() && started) || startNow){
     //byte r = rand(3);
     if(beat1[0][beatIndex]) aSample.start();
     if(beat1[1][beatIndex]) bSample.start();
@@ -53,15 +98,15 @@ void updateControl(){
     beatIndex = beatIndex % 16;
     kTriggerDelay.start(beatTime);
   }
-  if(false) lpf.setCutoffFreq(mozziAnalogRead(0)>>2);
-  if(false) {
+  if(!buttonA.read()) lpf.setCutoffFreq(mozziAnalogRead(0)>>2);
+  if(!buttonB.read()) {
     bitCrushLevel = 7-(mozziAnalogRead(0)>>7);
     bitCrushCompensation = bitCrushLevel;
     if(bitCrushLevel >= 6) bitCrushCompensation --;
     if(bitCrushLevel >= 7) bitCrushCompensation --;
   }
-  if(false) beatTime = 20 + mozziAnalogRead(0); // temp
-  if(true) {
+  if(!buttonC.read()) beatTime = 20 + mozziAnalogRead(0); // temp
+  if(!buttonD.read()) {
     aSample.setFreq(((float) mozziAnalogRead(0) / 255.0f) * (float) kick_SAMPLERATE / (float) kick_NUM_CELLS);
     bSample.setFreq(((float) mozziAnalogRead(0) / 255.0f) * (float) closedhat_SAMPLERATE / (float) closedhat_NUM_CELLS);
     cSample.setFreq(((float) mozziAnalogRead(0) / 255.0f) * (float) snare_SAMPLERATE / (float) snare_NUM_CELLS);
