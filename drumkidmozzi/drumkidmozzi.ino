4e054d619655f2ee917bd28bcd5229d4f6347849
Matt Bradshaw <matthewloydbradshaw@gmail.com>
2019-04-24 11:20:54 +0100

    design switched to 3 knobs, 5 buttons - changed code accordingly

--- drumkidmozzi/drumkidmozzi.ino
+++ drumkidmozzi/drumkidmozzi.ino
@@ -15,28 +15,24 @@
 
 // define some numbers etc
 #define CONTROL_RATE 64
-#define BUTTON_A_PIN 0
+#define START_STOP_PIN 2
+#define SHIFT_PIN 4
+#define BUTTON_A_PIN 7
 #define BUTTON_B_PIN 8
-#define BUTTON_C_PIN 1
-#define BUTTON_D_PIN 2
-#define BUTTON_E_PIN 4
-#define BUTTON_F_PIN 12
-#define BUTTON_G_PIN 7
+#define BUTTON_C_PIN 12
 
-#define LED_1 10
+#define LED_1 3
 #define LED_2 5
-#define LED_3 3
-#define LED_4 11
-#define LED_5 6
+#define LED_3 6
+#define LED_4 10
+#define LED_5 11
 
 // define buttons
 Bounce buttonA = Bounce();
 Bounce buttonB = Bounce();
 Bounce buttonC = Bounce();
-Bounce buttonD = Bounce();
-Bounce buttonE = Bounce();
-Bounce buttonF = Bounce();
-Bounce buttonG = Bounce();
+Bounce startStopButton = Bounce();
+Bounce shiftButton = Bounce();
 
 // define samples
 Sample <kick_NUM_CELLS, AUDIO_RATE> aSample(kick_DATA);
@@ -53,6 +49,15 @@ int beatTime = 150;
 byte beatIndex = 0;
 float tempo = 120;
 float blend = 0.5;
+byte controlSet = 0;
+byte ledPins[] = {LED_1,LED_2,LED_3,LED_4,LED_5};
+
+int storedValues[6][3] = {  {0,0,0},
+                            {0,0,0},
+                            {0,0,0},
+                            {0,0,0},
+                            {0,0,0},
+                            {0,0,0},};
 
 // define beats (temporarily here, probably move to a separate file later?)
 byte beat1[][16] = {  {255,0,0,0,0,0,0,0,255,0,255,0,64,128,212,255,},
@@ -66,6 +71,7 @@ byte beat2[][16] = {  {255,0,0,0,0,0,0,0,255,0,0,0,0,0,0,0,},
 byte blendedBeat[3][16];
 byte hyper = 0;
 
+
 void setup(){
   pinMode(LED_1,OUTPUT);
   pinMode(LED_2,OUTPUT);
@@ -73,34 +79,30 @@ void setup(){
   pinMode(LED_4,OUTPUT);
   pinMode(LED_5,OUTPUT);
   digitalWrite(LED_1,HIGH);
-  delay(20);
+  delay(200);
   digitalWrite(LED_1,LOW);
   digitalWrite(LED_2,HIGH);
-  delay(20);
+  delay(200);
   digitalWrite(LED_2,LOW);
   digitalWrite(LED_3,HIGH);
-  delay(20);
+  delay(200);
   digitalWrite(LED_3,LOW);
   digitalWrite(LED_4,HIGH);
-  delay(20);
+  delay(200);
   digitalWrite(LED_4,LOW);
   digitalWrite(LED_5,HIGH);
-  delay(20);
+  delay(200);
   digitalWrite(LED_5,LOW);
+  startStopButton.attach(START_STOP_PIN, INPUT_PULLUP);
+  shiftButton.attach(SHIFT_PIN, INPUT_PULLUP);
   buttonA.attach(BUTTON_A_PIN, INPUT_PULLUP);
   buttonB.attach(BUTTON_B_PIN, INPUT_PULLUP);
   buttonC.attach(BUTTON_C_PIN, INPUT_PULLUP);
-  buttonD.attach(BUTTON_D_PIN, INPUT_PULLUP);
-  buttonE.attach(BUTTON_E_PIN, INPUT_PULLUP);
-  buttonF.attach(BUTTON_F_PIN, INPUT_PULLUP);
-  buttonG.attach(BUTTON_G_PIN, INPUT_PULLUP);
+  startStopButton.interval(25);
+  shiftButton.interval(25);
   buttonA.interval(25);
   buttonB.interval(25);
   buttonC.interval(25);
-  buttonD.interval(25);
-  buttonE.interval(25);
-  buttonF.interval(25);
-  buttonG.interval(25);
   startMozzi(CONTROL_RATE);
   aSample.setFreq((float) kick_SAMPLERATE / (float) kick_NUM_CELLS);
   bSample.setFreq((float) closedhat_SAMPLERATE / (float) closedhat_NUM_CELLS);
@@ -121,10 +123,13 @@ void updateControl(){
   buttonA.update();
   buttonB.update();
   buttonC.update();
-  buttonD.update();
-  buttonE.update();
-  buttonF.update();
-  buttonG.update();
+  startStopButton.update();
+  shiftButton.update();
+
+  if(buttonA.fell()) controlSet = 0 + (!shiftButton.read()?3:0);
+  else if(buttonB.fell()) controlSet = 1 + (!shiftButton.read()?3:0);
+  else if(buttonC.fell()) controlSet = 2 + (!shiftButton.read()?3:0);
+  
   // attempt blending beat
   for(byte i=0;i<3;i++) {
     for(byte j=0;j<16;j++) {
@@ -140,35 +145,49 @@ void updateControl(){
     if(blendedBeat[0][beatIndex]>0) aVol = blendedBeat[0][beatIndex];
     if(blendedBeat[1][beatIndex]>0) bVol = blendedBeat[1][beatIndex];
     if(blendedBeat[2][beatIndex]>0) cVol = blendedBeat[2][beatIndex];
+    for(int i=0;i<5;i++) digitalWrite(ledPins[i], LOW);
+    if(beatIndex%4==0) digitalWrite(ledPins[beatIndex/4], HIGH);
     beatIndex ++;
     beatIndex = beatIndex % 16;
     kTriggerDelay.start(beatTime);
   }
-  if(buttonA.fell()) {
+  if(startStopButton.fell()) {
     started = !started;
     if(started) {
       beatIndex = 0;
       startNow = true;
     }
   }
-  if(!buttonB.read()) blend = mozziAnalogRead(0)/1023.0;
-  if(!buttonC.read()) hyper = mozziAnalogRead(0)>>2;
-  if(!buttonD.read()) lpf.setCutoffFreq(mozziAnalogRead(0)>>2);
-  if(!buttonE.read()) {
-    bitCrushLevel = 7-(mozziAnalogRead(0)>>7);
-    bitCrushCompensation = bitCrushLevel;
-    if(bitCrushLevel >= 6) bitCrushCompensation --;
-    if(bitCrushLevel >= 7) bitCrushCompensation --;
-  }
-  if(!buttonF.read()){
-    // could probably optimise this maths
-    tempo = 40.0 + mozziAnalogRead(0) / 5.0;
-    beatTime = 15.0 / (tempo/1000.0);
-  }
-  if(!buttonG.read()) {
+  switch(controlSet) {
+    case 0:
+    hyper = mozziAnalogRead(0)>>2;
+    //ceiling = mozziAnalogRead(1)>>2;
+    //zoom = mozziAnalogRead(2)>>2;
+    break;
+    case 1:
     aSample.setFreq(((float) mozziAnalogRead(0) / 255.0f) * (float) kick_SAMPLERATE / (float) kick_NUM_CELLS);
     bSample.setFreq(((float) mozziAnalogRead(0) / 255.0f) * (float) closedhat_SAMPLERATE / (float) closedhat_NUM_CELLS);
     cSample.setFreq(((float) mozziAnalogRead(0) / 255.0f) * (float) snare_SAMPLERATE / (float) snare_NUM_CELLS);
+    lpf.setCutoffFreq(mozziAnalogRead(1)>>2);
+    bitCrushLevel = 7-(mozziAnalogRead(2)>>7);
+    bitCrushCompensation = bitCrushLevel;
+    if(bitCrushLevel >= 6) bitCrushCompensation --;
+    if(bitCrushLevel >= 7) bitCrushCompensation --;
+    break;
+    case 2:
+    // could probably optimise this maths
+    tempo = 40.0 + mozziAnalogRead(0) / 5.0;
+    beatTime = 15.0 / (tempo/1000.0);
+    break;
+    case 3:
+
+    break;
+    case 4:
+
+    break;
+    case 5:
+    blend = mozziAnalogRead(0)/1023.0;
+    break;
   }
 }
 
