9f39249a687d0ea6babc5212b95b10c8655aa09c
Matt Bradshaw <matthewloydbradshaw@gmail.com>
2020-03-10 16:36:56 +0000

    aleatoric stuff starting to work

--- v6/arduino/drumkid_new/drumkid_new.ino
+++ v6/arduino/drumkid_new/drumkid_new.ino
@@ -4,7 +4,9 @@
 
 #include <MozziGuts.h>
 #include <Sample.h>
-//#include <mozzi_rand.h>
+#include <mozzi_rand.h>
+
+#include "beats.h"
 
 #include "kick.h"
 #include "closedhat.h"
@@ -50,17 +52,8 @@ byte noteDown = B00000000;
 bool syncReceived = false;
 
 const byte midiNotes[NUM_SAMPLES_TOTAL] = {36,42,38,37,43};
-
-// temp beat definition
-byte beats[1][5][4] = {
-  { // videotape bonnaroo
-    {B10000000,B10000010,B10000000,B10000010,},
-    {B10101010,B10111010,B10101010,B10111010,},
-    {B00001000,B00001000,B00001000,B00001000,},
-    {B00000000,B00001000,B00000000,B00001000,},
-    {B00000000,B00000100,B00000000,B00000100,},
-  },
-};
+const byte zoomValues[] = {32,16,8,4,2,1}; // could be compressed?
+byte sampleVolumes[NUM_SAMPLES_TOTAL] = {255,255,255,255,255}; // temp
 
 void setup(){
   byte i;
@@ -71,6 +64,7 @@ void setup(){
     pinMode(buttonPins[i],INPUT_PULLUP);
   }
   startMozzi(CONTROL_RATE);
+  randSeed();
   kick.setFreq((float) kick_SAMPLERATE / (float) kick_NUM_CELLS);
   closedhat.setFreq((float) closedhat_SAMPLERATE / (float) closedhat_NUM_CELLS);
   snare.setFreq((float) snare_SAMPLERATE / (float) snare_NUM_CELLS);
@@ -125,34 +119,86 @@ void doStartStop() {
     Serial.write(0xFA); // MIDI clock start
   } else {
     Serial.write(0xFC); // MIDI clock stop
+    updateLeds();
   }
 }
 
 void playPulseHits() {
   if(pulseNum % 6 == 0) {
     for(byte i=0; i<5; i++) {
-      if(bitRead(beats[0][i][stepNum/8],7-(stepNum%8))) {
-        switch(i) {
-          case 0:
-          kick.start();
-          break;
-          case 1:
-          closedhat.start();
-          break;
-          case 2:
-          snare.start();
-          break;
-          case 3:
-          rim.start();
-          break;
-          case 4:
-          tom.start();
-          break;
+      calculateNote(i);
+    }
+  }
+}
+
+byte paramZoom = 200;
+byte paramChance = 64;
+byte paramMidpoint = 127;
+byte paramRange = 127;
+byte paramBeat = 2;
+void calculateNote(byte sampleNum) {
+  byte zoomValueIndex = paramZoom / 51; // gives value from 0 to 5
+  byte zoomVelocity = paramZoom % 51; // 
+  if(zoomValueIndex == 5) {
+    zoomValueIndex = 4;
+    zoomVelocity = 51;
+  }
+  byte lowerZoomValue = zoomValues[zoomValueIndex]; // e.g. 8 for a quarter note
+  byte upperZoomValue = zoomValues[zoomValueIndex+1]; // e.g. 16 for a quarter note
+  long thisVelocity = 0;
+  if(stepNum%2==0) {
+    // beats only defined down to 16th notes not 32nd, hence %2
+    byte beatByte = pgm_read_byte(&beats[paramBeat][sampleNum][stepNum/16]);
+    if(bitRead(beatByte,7-((stepNum/2)%8))) thisVelocity = 255;
+  }
+  if(thisVelocity==0) {
+    // for steps not defined in beat, use algorithm to determine velocity
+    if(stepNum%upperZoomValue==0) {
+      // if step length is longer(?) than is affected by zoom
+      byte yesNoRand = rand(0,255);
+      if(yesNoRand < paramChance) {
+        //int velocityRand = rand(0,255);
+        //velocityRand = 255;
+        //thisVelocity = velocityRand;
+        //thisVelocity = paramMidpoint - paramRange/2 + ((velocityRand * paramRange)>>8);
+        int lowerBound = paramMidpoint - paramRange/2;
+        int upperBound = paramMidpoint + paramRange/2;
+        thisVelocity = rand(lowerBound, upperBound);
+        thisVelocity = constrain(thisVelocity,0,255);
+        if(stepNum%lowerZoomValue!=0) {
+          //Serial.println(thisVelocity);
+          thisVelocity = thisVelocity * 5 * zoomVelocity / 255;
+          //Serial.println(thisVelocity);
+          //Serial.println("");
         }
-        bitWrite(noteDown,i,true);
-        playMidiNote(midiNotes[i], 255);
       }
     }
+  } 
+  triggerNote(sampleNum, thisVelocity);
+}
+
+void triggerNote(byte sampleNum, byte velocity) {
+  if(velocity>8) { // don't bother with very quiet notes
+    switch(sampleNum) {
+      case 0:
+      kick.start();
+      break;
+      case 1:
+      closedhat.start();
+      break;
+      case 2:
+      snare.start();
+      break;
+      case 3:
+      rim.start();
+      break;
+      case 4:
+      tom.start();
+      break;
+    }
+    sampleVolumes[sampleNum] = velocity;
+    bitWrite(noteDown,sampleNum,true);
+    playMidiNote(midiNotes[sampleNum], velocity);
   }
 }
 
@@ -187,9 +233,17 @@ void cancelMidiNotes() {
   }
 }
 
+const byte atten = 9;
 int updateAudio(){
-  char asig = (kick.next() >> 2) + (closedhat.next() >> 3) + (snare.next() >> 2) + (rim.next() >> 1) + (tom.next() >> 2);
-  return asig; // return an int signal centred around 0
+  //char asig = (kick.next() >> 2) + (closedhat.next() >> 3) + (snare.next() >> 2) + (rim.next() >> 1) + (tom.next() >> 2);
+  //return asig; // return an int signal centred around 0
+
+  char asig = ((sampleVolumes[0]*kick.next())>>atten)+
+    ((sampleVolumes[1]*closedhat.next())>>atten)+
+    ((sampleVolumes[2]*snare.next())>>atten)+
+    ((sampleVolumes[3]*rim.next())>>atten)+
+    ((sampleVolumes[4]*tom.next())>>atten);
+  return asig;
 }
 
 byte thisMidiByte;
@@ -226,7 +280,7 @@ void updateLeds() {
         case 0:
         if(i==stepNum/4) digitalWrite(ledPins[i], HIGH);
         break;
-        case 1:
+        case 12:
         if(i==stepNum/4) digitalWrite(ledPins[i], LOW);
         break;
       }
