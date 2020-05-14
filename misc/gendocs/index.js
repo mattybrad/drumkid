cc30d2849000ee6fffffe1c02f84c3c0698376ad
Matt Bradshaw <matthewloydbradshaw@gmail.com>
2020-05-14 15:19:02 +0100

    messing with manual style

--- misc/gendocs/index.js
+++ misc/gendocs/index.js
@@ -1,9 +1,13 @@
 var markdownpdf = require("markdown-pdf")
   , fs = require("fs")
 
-markdownpdf().from("docs/v6/manual.md").to("manual.pdf", function () {
+var mdOptions = {
+  cssPath: "misc/gendocs/style.css"
+}
+
+markdownpdf(mdOptions).from("docs/v6/manual.md").to("manual.pdf", function () {
   console.log("Manual PDF generated");
-  markdownpdf().from("docs/v6/hackers_manual.md").to("hackers_manual.pdf", function () {
+  markdownpdf(mdOptions).from("docs/v6/hackers_manual.md").to("hackers_manual.pdf", function () {
     console.log("Hackers' manual PDF generated");
   })
 })
