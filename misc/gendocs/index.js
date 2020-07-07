079b0bbf8bc1ce0d76d7d5fed81f256d9ba56ec4
Matt Bradshaw <matthewloydbradshaw@gmail.com>
2020-07-07 14:19:03 +0100

    big update of the manual

--- misc/gendocs/index.js
+++ misc/gendocs/index.js
@@ -7,7 +7,4 @@ var mdOptions = {
 
 markdownpdf(mdOptions).from("docs/v6/manual.md").to("manual.pdf", function () {
   console.log("Manual PDF generated");
-  markdownpdf(mdOptions).from("docs/v6/hackers_manual.md").to("hackers_manual.pdf", function () {
-    console.log("Hackers' manual PDF generated");
-  })
 })
--- misc/hooks/pre-commit.sh
+++ misc/hooks/pre-commit.sh
@@ -2,5 +2,4 @@ git diff --cached --name-only | if grep --quiet "docs"
 then
   node misc/gendocs/index.js
   git add manual.pdf
-  git add hackers_manual.pdf
 fi
