c1990d73cac59f89ec8984ae27f7c4782e3b5d84
Matt Bradshaw <matthewloydbradshaw@gmail.com>
2020-11-03 17:00:56 +0000

    superarp makes proper notes now

--- arduino/altkid/superarp/superarp.ino
+++ arduino/altkid/superarp/superarp.ino
@@ -10,6 +10,20 @@ byte gain[4];
 byte channel = 0;
 unsigned long int nextNoteTime = 0;
 
+const float noteFreqs[88] = {
+  27.50, 29.14, 30.87, 32.70, 34.65, 36.71, 38.89, 41.20, 43.65, 46.25,
+  49.00, 51.91, 55.00, 58.27, 61.74, 65.41, 69.30, 73.42, 77.78, 82.41,
+  87.31, 92.50, 98.00, 103.83, 110.00, 116.54, 123.47, 130.81, 138.59,
+  146.83, 155.56, 164.81, 174.61, 185.00, 196.00, 207.65, 220.00, 233.08,
+  246.94, 261.63, 277.18, 293.66, 311.13, 329.63, 349.23, 369.99, 392.00,
+  415.30, 440.00, 466.16, 493.88, 523.25, 554.37, 587.33, 622.25, 659.26,
+  698.46, 739.99, 783.99, 830.61, 880.00, 932.33, 987.77, 1046.50, 1108.73,
+  1174.66, 1244.51, 1318.51, 1396.91, 1479.98, 1567.98, 1661.22, 1760.00,
+  1864.66, 1975.53, 2093.00, 2217.46, 2349.32, 2489.02, 2637.02, 2793.83,
+  2959.96, 3135.96, 3322.44, 3520.00, 3729.31, 3951.07, 4186.01
+};
+const byte majorScale[7] = {0,2,4,5,7,9,11};
+
 ADSR <CONTROL_RATE, CONTROL_RATE> env[4];
 Oscil <SQUARE_ANALOGUE512_NUM_CELLS, AUDIO_RATE> aSin1(SQUARE_ANALOGUE512_DATA);
 Oscil <SQUARE_ANALOGUE512_NUM_CELLS, AUDIO_RATE> aSin2(SQUARE_ANALOGUE512_DATA);
@@ -34,7 +48,8 @@ void updateControl(){
   if(millis()>=nextNoteTime) {
     for(byte i=0;i<4;i++) {
       if(bitRead(quadRand, i)) {
-        int randFreq = rand(60,500);
+        byte randNote = rand(5,50);
+        float randFreq = noteFreqs[majorScale[randNote%7]+12*(randNote/7)];
         if(i==0) aSin1.setFreq(randFreq);
         else if(i==1) aSin2.setFreq(randFreq);
         else if(i==2) aSin3.setFreq(randFreq);
@@ -42,7 +57,7 @@ void updateControl(){
         env[i].noteOn();
       }
     }
-    nextNoteTime += 50;
+    nextNoteTime += rand(10,100);
   }
   for(byte i=0;i<4;i++) {
     env[i].update();
